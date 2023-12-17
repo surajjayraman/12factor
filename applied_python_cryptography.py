0d4ce78fb8fc9c1fdea9587cc1e48fee69e9b753
surajjayraman <suraj.jayraman@gmail.com>
2023-12-17 14:58:20 -0500

    Update crypto with bruteforce tests

--- applied_python_cryptography.py
+++ applied_python_cryptography.py
@@ -88,5 +88,28 @@ print(ciphertext)
 print(str(shift_key))
 print(decrypted_cipher)
 
+# try cracking by bruteforce
+
+ciphertext = "rovvy, cebkt, ryg kbo, iye?"
+
+def caesar_bruteforce(ciphertext):
+
+    for shift_key in range(0, 25):
+        decrypted_plaintext = ""
+        for char in ciphertext:
+            if char.islower():
+                char_index = ord(char) - ord("a")
+                char_unshifted = (char_index - shift_key) % 26 + ord("a")
+                char_decrypted = chr(char_unshifted)
+                decrypted_plaintext += char_decrypted
+            else :
+                decrypted_plaintext += char
+        print("with a shift of" + str(shift_key) + "the decrypted text is: " + decrypted_plaintext)
+
+caesar_bruteforce(ciphertext)
+
+
+
+
 
 
