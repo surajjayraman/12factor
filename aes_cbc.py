277f9a0da4eb76799a706ba1cef0d6111a1c1831
surajjayraman <suraj.jayraman@gmail.com>
2023-12-19 09:08:06 -0500

    Add aes_cbc decrypt functionality

--- aes_cbc.py
+++ aes_cbc.py
@@ -8,4 +8,9 @@ def AES_CBC_encrypt(plaintext, AES_key, initialization_vector):
     ciphertext = AES_CBC_cipher.encrypt((pad(plaintext, AES.block_size)))
     return ciphertext
 
-#def AES_CBC_decrypt(ciphertext, AES_key, initialization_vector)
+def AES_CBC_decrypt(ciphertext, AES_key, initialization_vector):
+    AES_CBC_cipher = AES.new(AES_key, AES.MODE_CBC, iv=initialization_vector)
+    decrypted_plaintext = unpad(AES_CBC_cipher.decrypt(ciphertext), AES.block_size)
+    return decrypted_plaintext
+
+
