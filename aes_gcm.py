bc1cadb379de2b53b6272bf69b1286352d5a1bfb
surajjayraman <suraj.jayraman@gmail.com>
2023-12-20 07:56:06 -0500

    Update aes_gcm with encrypt decrypt

--- aes_gcm.py
+++ aes_gcm.py
@@ -5,4 +5,15 @@ from Crypto.Random import get_random_bytes
 def AES_GCM_encrypt(plaintext, AES_key, nonce):
 
     AES_GCM_cipher =  AES.new(AES_key, AES.MODE_GCM, nonce=nonce)
-    
+    ciphertext = AES_GCM_cipher.encrypt(plaintext)
+    return ciphertext
+
+def AES_GCM_decrypt(ciphertext, AES_key, nonce):
+
+    AES_GCM_cipher =  AES.new(AES_key, AES.MODE_GCM, nonce=nonce)
+    decrypted_plaintext = AES_GCM_cipher.decrypt(ciphertext)
+    return decrypted_plaintext
+
+
+
+
