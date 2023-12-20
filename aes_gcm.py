587dbbc79b37a32f0210878444a28f369e6a5e11
surajjayraman <suraj.jayraman@gmail.com>
2023-12-20 09:59:52 -0500

    Add aes_gcm tests

--- aes_gcm.py
+++ aes_gcm.py
@@ -14,6 +14,12 @@ def AES_GCM_decrypt(ciphertext, AES_key, nonce):
     decrypted_plaintext = AES_GCM_cipher.decrypt(ciphertext)
     return decrypted_plaintext
 
+plaintext = input("Enter plaintext: ").encode("ASCII")
+AES_key = get_random_bytes(16)
+nonce = get_random_bytes(12)
 
+ciphertext = AES_GCM_encrypt(plaintext, AES_key, nonce)
+
+print("Cyphertext: ", ciphertext);
 
 
