a376134fd1468e341ab69c28392fcbd9468ebc5b
surajjayraman <suraj.jayraman@gmail.com>
2023-12-22 15:24:40 -0500

    Update des_ctr with tests

--- des_ctr.py
+++ des_ctr.py
@@ -12,5 +12,17 @@ def DES_CTR_decrypt(ciphertext, DES_key, nonce):
 
     DES_CTR_cipher =  DES.new(DES_key, DES.MODE_CTR, nonce=nonce)
     decrypted_plaintext = DES_CTR_cipher.decrypt(ciphertext)
-    
+
     return decrypted_plaintext
+
+
+plaintext = input("Enter plaintext: ").encode()
+DES_key = get_random_bytes(8)
+nonce = get_random_bytes(7)
+
+ciphertext = DES_CTR_encrypt(plaintext, DES_key, nonce)
+decrypted_plaintext = DES_CTR_decrypt(ciphertext, DES_key, nonce)
+
+print("Plaintext: ", plaintext.decode())
+print("Cyphertext: ", ciphertext.hex())
+print("Decrypted Plaintext: ", decrypted_plaintext.decode())
