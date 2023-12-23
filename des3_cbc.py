bba9200703c652aff5e0e44adca93e5315862e9f
surajjayraman <suraj.jayraman@gmail.com>
2023-12-23 12:22:12 -0500

    Add des3_cbc encrypt

--- des3_cbc.py
+++ des3_cbc.py
@@ -1,3 +1,11 @@
 from Crypto.Cipher import DES3
 from Crypto.Util.Padding import unpad, pad
 from Crypto.Random import get_random_bytes
+
+def DES3_CBC_encrypt(plaintext, DES3_key, initialization_vector):
+
+    DES3_CBC_cipher = DES3.new(DES3_key, DES3.MODE_CBC, iv=initialization_vector)
+    ciphertext = DES3_CBC_cipher.encrypt((pad(plaintext, DES3.block_size)))
+    
+    return ciphertext
+
