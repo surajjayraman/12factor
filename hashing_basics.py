24fdb5115a8d5e1a39f77d594c804c16a0abf87d
surajjayraman <suraj.jayraman@gmail.com>
2023-12-25 15:12:57 -0500

    Add sha256 pwd  hashing

--- hashing_basics.py
+++ hashing_basics.py
@@ -13,3 +13,20 @@ def hash(plaintext):
 
 plaintext = input("Enter plain text: ").encode("ASCII")
 hash(plaintext)
+
+# check for password match
+def sha256_hash_password(example_password, input_password):
+
+    example_sha256_hash = hashlib.sha256(example_password).hexdigest()
+    input_sha256_hash = hashlib.sha256(input_password).hexdigest()
+
+    print("Example password: ", example_password.decode("ASCII"))
+    print("Example SHA256 hash: ", example_sha256_hash)
+    print("Input password: ",input_password)
+    print("Input SHA256 hash", input_sha256_hash)
+
+
+example_password = input("Enter example password: ").encode("ASCII")
+input_password = input("Enter input password: ").encode("ASCII")
+
+sha256_hash_password(example_password, input_password)
