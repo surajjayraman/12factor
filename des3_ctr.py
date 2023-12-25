345c0c8767f995276be8cea77216a622c0d1b4ea
surajjayraman <suraj.jayraman@gmail.com>
2023-12-24 19:28:27 -0500

    Add des3_ctr tets

--- des3_ctr.py
+++ des3_ctr.py
@@ -3,14 +3,25 @@ from Crypto.Random import get_random_bytes
 
 def DES3_CTR_encrypt(plaintext, DES3_key, nonce):
 
-    DES3_CTR_cipher =  DES3.new(DES3_key, DES.MODE_CTR, nonce=nonce)
+    DES3_CTR_cipher =  DES3.new(DES3_key, DES3.MODE_CTR, nonce=nonce)
     ciphertext = DES3_CTR_cipher.encrypt(plaintext)
 
     return ciphertext
 
-def DES3_CTR_decrypt(ciphertext, DES_key, nonce):
+def DES3_CTR_decrypt(ciphertext, DES3_key, nonce):
 
-    DES3_CTR_cipher =  DES3.new(DES_key, DES3.MODE_CTR, nonce=nonce)
+    DES3_CTR_cipher =  DES3.new(DES3_key, DES3.MODE_CTR, nonce=nonce)
     decrypted_plaintext = DES3_CTR_cipher.decrypt(ciphertext)
 
     return decrypted_plaintext
+
+plaintext = input("Enter plaintext: ").encode()
+DES3_key = DES3.adjust_key_parity(get_random_bytes(24))
+nonce = get_random_bytes(7)
+
+ciphertext = DES3_CTR_encrypt(plaintext, DES3_key, nonce)
+decrypted_plaintext = DES3_CTR_decrypt(ciphertext, DES3_key, nonce)
+
+print("Plaintext: ", plaintext.decode())
+print("Cyphertext: ", ciphertext.hex())
+print("Decrypted Plaintext: ", decrypted_plaintext.decode())
