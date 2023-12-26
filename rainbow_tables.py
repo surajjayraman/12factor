c0a89cea60d1cee75463025818ec12f4ec97be44
surajjayraman <suraj.jayraman@gmail.com>
2023-12-26 18:38:01 -0500

    Update rainbow_table data structure

--- rainbow_tables.py
+++ rainbow_tables.py
@@ -1,3 +1,12 @@
 import hashlib
 
 rainbow_table = {}
+
+common_passwords = ['password', 'admin', 'letmein', '123456','test']
+
+for password in common_passwords:
+
+    hashvalue =  hashlib.sha256(password.encode()).hexdigest()
+    rainbow_table[hashvalue] = password
+
+print(rainbow_table)
