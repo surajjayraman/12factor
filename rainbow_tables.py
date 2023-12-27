1452c094fb1c953f06f75a3f9363ccf64b797fde
surajjayraman <suraj.jayraman@gmail.com>
2023-12-26 19:13:19 -0500

    Add rainbow_table tests

--- rainbow_tables.py
+++ rainbow_tables.py
@@ -10,3 +10,12 @@ for password in common_passwords:
     rainbow_table[hashvalue] = password
 
 print(rainbow_table)
+
+# crack input password
+password_to_crack = input("Please enter the password you wish to crack: ")
+hashed_password = hashlib.sha256(password_to_crack.encode()).hexdigest()
+
+if hashed_password in rainbow_table:
+    print("Password found for hash")
+else:
+    print("Password not found in hash")
