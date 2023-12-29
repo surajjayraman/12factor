197a0e39f19d7e037615485b060734d542fa4cc8
surajjayraman <suraj.jayraman@gmail.com>
2023-12-29 14:31:25 -0500

    Add diffie_hellman tests

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -8,3 +8,14 @@ alice_private_key = randint(1, prime -1 )
 
 print("bob_private_key: ", bob_private_key)
 print("alice_private_key: ", alice_private_key)
+
+bob_public_key = pow(primitive_root, bob_private_key, prime)
+alice_public_key = pow(primitive_root, alice_private_key, prime)
+
+shared_secret_key_bob = pow(alice_public_key, bob_private_key, prime)
+shared_secret_key_alice = pow(bob_public_key, alice_private_key, prime)
+
+if (shared_secret_key_bob == shared_secret_key_alice):
+    print("Secret keys match!")
+else:
+    print("Secret keys do not match")
