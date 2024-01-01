98f8ac88337c6f913f6797930580b6e95e76abcc
surajjayraman <suraj.jayraman@gmail.com>
2023-12-31 19:25:48 -0500

    Add generate_mac example

--- macs_sign.py
+++ macs_sign.py
@@ -4,4 +4,14 @@ from Crypto.Signature import pkcs1_15
 from Crypto.hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
 
+def generate_mac(symmetric_key, nonce, plaintext):
+
+    symmetric_cipher =  AES.new(symmetric_key, AES.MODE_GCM, nonce=nonce)
+    mac = symmetric_cipher.encrypt(plaintext)
+
+    return mac
+
+
+
+
 
